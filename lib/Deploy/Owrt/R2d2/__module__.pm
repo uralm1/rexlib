c1481739b71ce1d4f2807e4860ff06e12d8e9cee
uralm1 <uralm1@gmail.com>
2020-12-28 15:29:00 +0500

    Owrt r2d2 installation

--- lib/Deploy/Owrt/R2d2/__module__.pm
+++ lib/Deploy/Owrt/R2d2/__module__.pm
@@ -3,6 +3,7 @@ package Deploy::Owrt::R2d2;
 use Rex -feature=>['1.4'];
 use Data::Dumper;
 
+use FindBin;
 use Ural::Deploy::ReadDB_Owrt;
 use Ural::Deploy::Utils qw(:DEFAULT is_x86);
 
@@ -30,17 +31,50 @@ task "configure", sub {
   say "Updating package database.";
   update_package_db;
   say "Installing / updating packages for R2d2.";
-  for (qw/tc kmod-sched perl/) {
-    #perlbase-encode perlbase-findbin perl-dbi perl-dbd-mysql perl-netaddr-ip perl-sys-runalone libmariadb
+  for (qw/tc kmod-sched perl make perlbase-extutils perlbase-version/) {
+    #perlbase-encode perlbase-findbin perl-netaddr-ip 
     pkg $_, ensure => latest,
       on_change => sub { say "package $_ was installed." };
   }
 
-  #file "/etc/r2d2",
-  #  owner => "ural",
-  #  group => "root",
-  #  mode => 755,
-  #  ensure => "directory";
+  file "/etc/r2d2",
+    ensure => "absent";
+
+  file "/tmp/src",
+    ensure => "absent";
+
+  file "/tmp/src",
+    owner => "ural",
+    group => "root",
+    mode => 755,
+    ensure => "directory";
+
+  my $SOURCE_TAR = "files/gwsyn-latest.tar.gz";
+  my $dest_tar = "/tmp/src/gwsyn.tar.gz";
+  file $dest_tar,
+    owner => "ural",
+    group => "root",
+    mode => 644,
+    source => $SOURCE_TAR;
+
+  extract $dest_tar,
+    to => '/tmp/src/';
+
+  my @srcdir = grep {is_dir($_)} glob('/tmp/src/*');
+  die "Can't determine source upload path" unless @srcdir;
+  my @r = run "perl Makefile.PL", cwd => $srcdir[0], auto_die => TRUE;
+  say $_ for @r;
+  @r = run "make install", cwd => $srcdir[0], auto_die => TRUE;
+  say $_ for @r;
+
+  # clean up
+  file "/tmp/src",
+    ensure => "absent";
+
+  for (qw/make perlbase-extutils perlbase-version/) {
+    pkg $_, ensure => absent,
+      on_change => sub { say "package $_ was removed." };
+  }
 
   say 'R2d2 configuration finished for '.$p->get_host;
 };
